a9729d4c1938df009ec613e563f4022d601feef6
Samuel Burnham <samuel.burnham@gmail.com>
2022-04-22 21:58:36 -0400

    Fix CI

--- src/repl.rs
+++ src/repl.rs
@@ -283,7 +283,7 @@ impl ReplState {
                             Tag::Str => {
                                 let path = store.fetch(&s).unwrap();
                                 let path = PathBuf::from(path.as_str().unwrap());
-                                self.handle_load(path, println)?;
+                                self.handle_load(&mut store, path, println)?;
                                 (true, true)
                             }
                             other => {
@@ -325,14 +325,16 @@ impl ReplState {
         Ok(result)
     }
 
-    pub fn handle_load<P: AsRef<Path>>(&mut self, path: P, println: &dyn Fn(String)) -> Result<()> {
+    pub fn handle_load<P: AsRef<Path>>(
+        &mut self,
+        store: &mut Store<Fr>,
+        path: P,
+        println: &dyn Fn(String),
+    ) -> Result<()> {
         println(format!("Loading from {}.", path.as_ref().to_str().unwrap()));
         let input = read_to_string(path)?;
-
-        let store_mutex = self.store.clone();
-        let mut store = store_mutex.lock().unwrap();
         let expr = store.read(&input).unwrap();
-        let (result, _limit, _next_cont) = self.eval_expr(expr, &mut store);
+        let (result, _limit, _next_cont) = self.eval_expr(expr, store);
 
         self.env = result;
 
@@ -370,7 +372,7 @@ impl ReplState {
                                     Expression::Str(path) => {
                                         let joined =
                                             p.as_ref().parent().unwrap().join(Path::new(&path));
-                                        self.handle_load(&joined, println)?
+                                        self.handle_load(&mut store, &joined, println)?
                                     }
                                     _ => panic!("Argument to :LOAD must be a string."),
                                 }
